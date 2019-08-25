9f4abc95b8e3ec1c31eec6b64c27a95a4b50e039
meganerisu <legoizumi_show@yahoo.co.jp>
2019-08-25 22:24:14 +0900

    SignInSceneを作ったよ

---
+++ IKY/Assets/Scripts/SignInButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+
+public class SignInButton : MonoBehaviour
+{
+    public InputField addressField;
+    public InputField passwordField;
+    private string email;
+    private string password;
+    Firebase.Auth.FirebaseAuth auth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        addressField = addressField.GetComponent<InputField>();
+        passwordField = passwordField.GetComponent<InputField>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SignIn(){
+
+        email = addressField.text;
+        password = passwordField.text;
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+        if (task.IsCanceled) {
+            Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+            return;
+        }
+        if (task.IsFaulted) {
+            Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+            return;
+        }
+
+        Firebase.Auth.FirebaseUser newUser = task.Result;
+        Debug.LogFormat("User signed in successfully: {0} ({1})",
+            newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
---
+++ IKY/Assets/Scripts/SignUpButton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+
+public class SignUpButton : MonoBehaviour
+{
+    public InputField addressField;
+    public InputField passwordField;
+
+    private string email;
+    private string password;
+
+    Firebase.Auth.FirebaseAuth auth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        addressField = addressField.GetComponent<InputField>();
+        passwordField = passwordField.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SignUp(){
+
+        email = addressField.text;
+        password = passwordField.text;
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+        if (task.IsCanceled) {
+            Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+            return;
+        }
+        if (task.IsFaulted) {
+            Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+            return;
+        }
+
+        // Firebase user has been created.
+        Firebase.Auth.FirebaseUser newUser = task.Result;
+        Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+            newUser.DisplayName, newUser.UserId);
+
+        SceneManager.LoadScene("SignedInScene");
+        });
+    }
+
+}
