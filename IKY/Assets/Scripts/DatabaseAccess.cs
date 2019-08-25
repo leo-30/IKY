694ac1cc542feeb2e11696788bda8e13eec7e083
leo-30 <mio.rabbit971.snow@gmail.com>
2019-08-25 21:34:38 +0900

    ブランチ切った。

---
+++ IKY/Assets/Scripts/DatabaseAccess.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class DatabaseAccess : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://YOUR-FIREBASE-APP.firebaseio.com/");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
